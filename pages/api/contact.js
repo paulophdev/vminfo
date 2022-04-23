dfba6430366b631d8c3317be0882c1aa65e3403a
paulophdev <83602060+paulophdev@users.noreply.github.com>
2022-04-23 08:48:04 -0300

    up

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,10 @@
 export default function handler(req, res) {
   let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
+    service: "gmail",
     host: "smtp.gmail.com",
     port: 465,
-    secure: false,
+    secure: true,
     auth: {
       user: "paulophdev@gmail.com",
       pass: "paulo3660",
