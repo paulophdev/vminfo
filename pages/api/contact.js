d7ee1631f6e013548ce7e280a109b327f1188e6f
paulophdev <83602060+paulophdev@users.noreply.github.com>
2022-04-23 09:17:04 -0300

    up

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,15 +1,14 @@
 export default function handler(req, res) {
   let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
-    secure: true,
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
     auth: {
-      user: "paulophdev@gmail.com",
-      pass: "maxT78XA0cf25wQj",
+      user: "334d982d2ec0bb",
+      pass: "daec23443013dc",
     },
   });
-
   const mailData = {
     from: "paulophdev@gmail.com",
     to: "paulophdev@gmail.com",
@@ -23,7 +22,6 @@ export default function handler(req, res) {
       Mensagem: ${req.body.msg}
     </div>`,
   };
-
   transporter.sendMail(mailData, function (err, info) {
     if (err) {
       res.status(400).json({ err });
